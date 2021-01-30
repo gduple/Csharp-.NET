a161ee246a0157f68104a19022a0cb93ba6150f3
gduple <gduple@gmail.com>
2021-01-30 17:32:34 -0600

    through pg 200

--- Chapter06/PacktLibrary/Person.cs
+++ Chapter06/PacktLibrary/Person.cs
@@ -108,5 +108,22 @@ namespace Packt.Shared
             }
         }
     }
-    
+
+    public class Thing
+    {
+        public object Data = default(object);
+
+        public string Process(object input)
+        {
+            if (Data == input)
+            {
+                return "Data and input are the same.";
+            }
+            else
+            {
+                return "data and input are NOT the same.";
+            }
+        }
+    }
+
 }
