c64a1b086ec0c53d821c870a3c8e63848243d27c
gduple <gduple@gmail.com>
2021-01-30 18:45:35 -0600

    generic types

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -74,7 +74,13 @@ namespace PeopleApp
             t2.Data = "apple";
             WriteLine($"Thing with a string: {t2.Process("apple")}");
 
+            var gt1 = new GenericThing<int>();
+            gt1.Data = 42;
+            WriteLine($"GenericThing with an integer: {gt1.Process(42)}");
 
+            var gt2 = new GenericThing<string>();
+            gt2.Data = "apple";
+            WriteLine($"GenericThing with a string: {gt2.Process("apple")}");
         }
         private static void Harry_Shout(object sender, EventArgs e)
         {
