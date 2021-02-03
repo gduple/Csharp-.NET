eb27d9ab0af584fd087cd7b5bf0596130cce0865
gduple <gduple@gmail.com>
2021-02-02 21:38:38 -0600

    Ch 8 setup, .NET types, working with numbers

---
+++ Chapter08/WorkingWithNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+using System.Numerics;
+
+namespace WorkingWithNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var largest = ulong.MaxValue;
+            WriteLine($"{largest,40:N0}");
+        }
+    }
+}
