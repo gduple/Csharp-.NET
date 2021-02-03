761a2ec3a9029fabc334cb332004db9281069148
gduple <gduple@gmail.com>
2021-02-02 22:06:55 -0600

    string methods

---
+++ Chapter08/WorkingWithText/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace WorkingWithText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "London";
+            WriteLine($"{city} is {city.Length} characters long.");
+
+            WriteLine($"First char is {city[0]} and third is {city[2]}.");
+
+            string cities = "Paris,Berlin,Madrid,New York";
+
+            string[] citiesArray = cities.Split(',');
+
+            foreach (string item in citiesArray)
+            {
+                WriteLine(item);
+            }
+        }
+    }
+}
