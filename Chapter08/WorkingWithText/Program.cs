c9f42f6a53ad1ca8b4385e3861bb7c22dcc4f449
gduple <gduple@gmail.com>
2021-02-02 23:29:58 -0600

    more string methods, format

--- Chapter08/WorkingWithText/Program.cs
+++ Chapter08/WorkingWithText/Program.cs
@@ -43,6 +43,17 @@ namespace WorkingWithText
             bool endsWithT = company.EndsWith("t");
 
             WriteLine($"Starts with M: {startsWithM}, contains an N: {containsN}, ends with T: {endsWithT}");
+
+            string recombined = string.Join(" => ", citiesArray);
+            WriteLine(recombined);
+
+            string fruit = "Apples";
+            decimal price = 0.39M;
+            DateTime when = DateTime.Today;
+
+            WriteLine($"{fruit} cost {price:C} on {when:dddd}s");
+            WriteLine(string.Format("{0} cost {1:C} on {2:dddd}s.",
+                fruit, price, when));
                 
         }
     }
