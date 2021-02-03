66c8e8e80b4787490eab35e238f774c57022353d
gduple <gduple@gmail.com>
2021-02-02 21:43:44 -0600

    working with big integers

--- Chapter08/WorkingWithNumbers/Program.cs
+++ Chapter08/WorkingWithNumbers/Program.cs
@@ -10,6 +10,10 @@ namespace WorkingWithNumbers
         {
             var largest = ulong.MaxValue;
             WriteLine($"{largest,40:N0}");
+
+            var atomsInTheUniverse =
+                BigInteger.Parse("123456789012345678901234567890");
+            WriteLine($"{atomsInTheUniverse,40:N0}");
         }
     }
 }
