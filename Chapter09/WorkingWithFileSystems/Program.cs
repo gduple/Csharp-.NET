57aef4b8a690812a4f5ca627fb5792a84a314de6
gduple <gduple@gmail.com>
2021-02-04 00:44:11 -0600

    managing files

--- Chapter09/WorkingWithFileSystems/Program.cs
+++ Chapter09/WorkingWithFileSystems/Program.cs
@@ -13,7 +13,8 @@ namespace WorkingWithFileSystems
         {
             // OutputFileSystemInfo();
             // WorkWithDrives();
-            WorkWithDirectories();
+            // WorkWithDirectories();
+            WorkWithFiles();
         }
         static void OutputFileSystemInfo()
         {
@@ -74,6 +75,51 @@ namespace WorkingWithFileSystems
             WriteLine("Deleting it...");
             Delete(newFolder, recursive: true);
             WriteLine($"Does it exist? {Exists(newFolder)}");
+        }
+
+        static void WorkWithFiles()
+        {
+            // define a directory path to output files
+            // starting in the user's folder
+            var dir = Combine(
+                GetFolderPath(SpecialFolder.Personal),
+                "Csharp-.NET", "Chapter09", "OutputFiles"
+            );
+
+            CreateDirectory(dir);
+
+            // define file paths
+            string textFile = Combine(dir, "Dummy.txt");
+            string backupFile = Combine(dir, "Dummy.bak");
+            WriteLine($"Working with: {textFile}");
+
+            // check if a file exists
+            WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+            // create a new text file and write a line to it
+            StreamWriter textWriter = File.CreateText(textFile);
+            textWriter.WriteLine("Hello, C#!");
+            textWriter.Close(); // close file and release resources
+            WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+            // copy the file, and overwrite it if it already exists
+            File.Copy(sourceFileName: textFile,
+                destFileName: backupFile, overwrite: true);
+            WriteLine(
+                $"Does {backupFile} exist? {File.Exists(backupFile)}"
+            );
+            Write("Confirm the file exists, and then press ENTER: ");
+            ReadLine();
+
+            // delete file
+            File.Delete(textFile);
+            WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+            // read from the text file backup
+            WriteLine($"Reading contents of {backupFile}:");
+            StreamReader textReader = File.OpenText(backupFile);
+            WriteLine(textReader.ReadToEnd());
+            textReader.Close();
         }
     }
 }
