18ef0c9583d0aebbd86e72ce0affe8094523f71c
gduple <gduple@gmail.com>
2021-02-04 00:54:25 -0600

    managing paths, getting file information

--- Chapter09/WorkingWithFileSystems/Program.cs
+++ Chapter09/WorkingWithFileSystems/Program.cs
@@ -120,6 +120,21 @@ namespace WorkingWithFileSystems
             StreamReader textReader = File.OpenText(backupFile);
             WriteLine(textReader.ReadToEnd());
             textReader.Close();
+
+            // Managing paths
+            WriteLine($"Folder Name: {GetDirectoryName(textFile)}");
+            WriteLine($"File Name: {GetFileName(textFile)}");
+            WriteLine("File Name without Extension: {0}",
+                GetFileNameWithoutExtension(textFile));
+            WriteLine($"File Extension: {GetExtension(textFile)}");
+            WriteLine($"Random File Name: {GetRandomFileName()}");
+            WriteLine($"Temporary File Name: {GetTempFileName()}");
+
+            var info = new FileInfo(backupFile);
+            WriteLine($"{backupFile}:");
+            WriteLine($"Contains {info.Length} bytes");
+            WriteLine($"Last accessed {info.LastAccessTime}");
+            WriteLine($"Has readonly set to {info.IsReadOnly}");
         }
     }
 }
