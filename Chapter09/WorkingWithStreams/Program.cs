58af6396198aa728996deb16d8e6a5377ba48e20
gduple <gduple@gmail.com>
2021-02-04 14:22:24 -0600

    writing to text streams

---
+++ Chapter09/WorkingWithStreams/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+
+namespace WorkingWithStreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkWithText();
+        }
+
+        static string[] callsigns = new string[] {
+            "Husker", "Starbuck", "Apollo", "Boomer",
+            "Bulldog", "Athena", "Helo", "Racetrack"
+        };
+
+        static void WorkWithText()
+        {
+            // define a file to write to
+            string textFile = Combine(CurrentDirectory, "streams.txt");
+            
+            // create a text file and return a helper writer
+            StreamWriter text = File.CreateText(textFile);
+
+            // enumerate the strings, writing each one
+            // to the stream on a separate line
+            foreach (string item in callsigns)
+            {
+                text.WriteLine(item);
+            }
+            text.Close(); // release resources
+
+            // output the contents of the file
+            WriteLine("{0} contains {1:N0} bytes.",
+                arg0: textFile,
+                arg1: new FileInfo(textFile).Length
+            );
+            WriteLine(File.ReadAllText(textFile));
+        }
+    }
+}
