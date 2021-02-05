c72735a4b56c09fdc2c11219cf76eedcd784865c
gduple <gduple@gmail.com>
2021-02-05 01:48:00 -0600

    encrypting symmetrically with AES

--- Chapter10/CryptographyLib/Protector.cs
+++ Chapter10/CryptographyLib/Protector.cs
@@ -1,8 +1,82 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Xml.Linq;
+using static System.Convert;
 
-namespace CryptographyLib
+// namespace CryptographyLib
+namespace Packt.Shared
 {
-    public class Class1
+    public static class Protector
     {
+        // salt size must be at least 8 bytes, we will use 16 bytes
+        private static readonly byte[] salt =
+            Encoding.Unicode.GetBytes("7BANANAS");
+
+        // iterations should be high enough to take at least 100ms to
+        // generate a Key and IV on the target machine.5 50,000 iterations
+        // takes 131ms on my 3.3 GHz Dual-Core Intel Core i7 MacBook Pro
+        private static readonly int iterations = 50_000;
+
+        public static string Encrypt(
+            string plainText, string password)
+        {
+            byte[] encryptedBytes;
+            byte[] plainBytes = Encoding.Unicode.GetBytes(plainText);
+
+            var aes = Aes.Create(); // abstract class factory method
+
+            var pbkdf2 = new Rfc2898DeriveBytes(
+                password, salt, iterations
+            );
+
+            aes.Key = pbkdf2.GetBytes(32); // set a 256-bit key
+            aes.IV = pbkdf2.GetBytes(16); // set a 128-bit key
+
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(
+                    ms, aes.CreateEncryptor(),
+                    CryptoStreamMode.Write))
+                {
+                    cs.Write(plainBytes, 0, plainBytes.Length);
+                }
+                encryptedBytes = ms.ToArray();
+            }
+            return Convert.ToBase64String(encryptedBytes);
+        }
+
+            public static string Decrypt(
+                string cryptoText, string password)
+            {
+                byte[] plainBytes;
+                byte[] cryptoBytes = Convert.
+                    FromBase64String(cryptoText);
+
+                var aes = Aes.Create();
+
+                var pbkdf2 = new Rfc2898DeriveBytes(
+                    password, salt, iterations);
+
+                aes.Key = pbkdf2.GetBytes(32);
+                aes.IV = pbkdf2.GetBytes(16);
+
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(
+                        ms, aes.CreateDecryptor(),
+                        CryptoStreamMode.Write))
+                    {
+                        cs.Write(cryptoBytes, 0, cryptoBytes.Length);
+                    }
+                    plainBytes = ms.ToArray();
+                }
+                return Encoding.Unicode.GetString(plainBytes);
+            }
+            
+        
     }
 }
--- Chapter10/EncryptionApp/Program.cs
+++ Chapter10/EncryptionApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography; // CryptographicException
+using Packt.Shared;                 // Protector
 using static System.Console;
 
 namespace EncryptionApp
@@ -7,7 +9,36 @@ namespace EncryptionApp
     {
         static void Main(string[] args)
         {
-            
+            Write("Enter a message that you want to encrypt: ");
+            string message = ReadLine();
+
+            Write("Enter a password: ");
+            string password = ReadLine();
+
+            string cryptoText = Protector.Encrypt(message, password);
+
+            WriteLine($"Encrypted text: {cryptoText}");
+
+            Write("Enter the password: ");
+            string password2 = ReadLine();
+
+            try
+            {
+                string clearText = Protector.Decrypt(cryptoText, password2);
+                WriteLine($"Decrypted text: {clearText}");
+            }
+            catch (CryptographicException ex)
+            {
+                WriteLine("{0}\nMore details: {1}",
+                    arg0: "You entered the wrong password!",
+                    arg1: ex.Message);
+            }
+            catch (Exception ex)
+            {
+                WriteLine("Non-cryptographic exceptions: {0}, {1}",
+                    arg0: ex.GetType().Name,
+                    arg1: ex.Message);
+            }
         }
     }
 }
