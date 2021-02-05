ec972009f13d4a003b8bb1a4fa66fe1cd1cdff44
gduple <gduple@gmail.com>
2021-02-05 00:41:21 -0600

    Chapter 10 setup redone

---
+++ Chapter10/EncryptionApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace EncryptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
