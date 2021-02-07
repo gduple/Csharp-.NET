e672f17c1a3fa52b4e2d6e4b68cb18fc978bd821
gduple <gduple@gmail.com>
2021-02-07 13:57:59 -0600

    Ch 12, LINQ queries, filtering with Where

---
+++ Chapter12/LinqWithObjects/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace LinqWithObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinqWithArrayOfStrings();
+        }
+
+        static void LinqWithArrayOfStrings()
+        {
+            var names = new string[] { "Michael", "Pam", "Jim", "Dwight",
+                "Angela", "Kevin", "Toby", "Creed" };
+            var query = names.Where(new Func<string, bool>(NameLongerThanFour));
+
+            foreach (string item in query)
+            {
+                WriteLine(item);
+            }
+        }
+
+       static bool NameLongerThanFour(string name)
+            {
+                return name.Length > 4;
+            }
+    }
+}
