96a2a10d976d49af33f6c4ac41277db10e0db646
gduple <gduple@gmail.com>
2021-02-06 19:31:58 -0600

    inserting

--- Chapter11/WorkingWithEFCore/Program.cs
+++ Chapter11/WorkingWithEFCore/Program.cs
@@ -10,9 +10,14 @@ namespace WorkingWithEFCore
     {
         static void Main(string[] args)
         {
-            QueryingCategories();
+            // QueryingCategories();
             // FilteredIncludes();
             // QueryingProducts();
+            if (AddProduct(6, "Bob's Burgers", 500M))
+            {
+                WriteLine("Add product successful.");
+            }
+            ListProducts();
         }
         
         static void QueryingCategories()
@@ -109,6 +114,43 @@ namespace WorkingWithEFCore
                     WriteLine(
                         "{0}: {1} costs {2:$#,##0.00} and has {3} in stock.",
                         item.ProductID, item.ProductName, item.Cost, item.Stock);
+                }
+            }
+        }
+
+        static bool AddProduct(
+            int categoryID, string productName, decimal? price)
+        {
+            using (var db = new Northwind())
+            {
+                var newProduct = new Product
+                {
+                    CategoryID = categoryID,
+                    ProductName = productName,
+                    Cost = price
+                };
+
+                // mark product as added in change tracking
+                db.Products.Add(newProduct);
+
+                // save tracked change to database
+                int affected = db.SaveChanges();
+                return (affected == 1);
+            }
+        }
+
+        static void ListProducts()
+        {
+            using (var db = new Northwind())
+            {
+                WriteLine("{0,-3} {1,-35} {2,8} {3,5} {4}",
+                    "ID", "Product Name", "Cost", "Stock", "Disc.");
+
+                foreach (var item in db.Products.OrderByDescending(p => p.Cost))
+                {
+                    WriteLine("{0:000} {1,-35} {2,8:$#,##0.00} {3,5} {4}",
+                        item.ProductID, item.ProductName, item.Cost,
+                        item.Stock, item.Discontinued);
                 }
             }
         }
