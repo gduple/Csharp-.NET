6fb56a2884bc20fcf44a77243ea85390a5039000
gduple <gduple@gmail.com>
2021-02-08 17:31:27 -0600

    Ch 13 setup

---
+++ Chapter13/MonitoringApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Console;
+using static System.Diagnostics.Process;
+
+namespace MonitoringApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
