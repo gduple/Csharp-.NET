1fc178b6e06a5904d34f97ed0e6206d23159ecbc
gduple <gduple@gmail.com>
2021-02-08 21:23:26 -0600

    running multiple actions synchronously

---
+++ Chapter13/WorkingWithTasks/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Console;
+
+namespace WorkingWithTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = Stopwatch.StartNew();
+            WriteLine("Running methods synchronously on one thread.");
+            MethodA();
+            MethodB();
+            MethodC();
+            WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+        }
+
+        static void MethodA()
+        {
+            WriteLine("Starting Method A...");
+            Thread.Sleep(3000); // simulate three seconds of work
+            WriteLine("Finished Method A.");
+        }
+
+        static void MethodB()
+        {
+            WriteLine("Starting Method A...");
+            Thread.Sleep(2000);
+            WriteLine("Finished Method B.");
+        }
+
+        static void MethodC()
+        {
+            WriteLine("Starting Method C...");
+            Thread.Sleep(1000);
+            WriteLine("Finished Method C.");
+        }
+    }
+}
