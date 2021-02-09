20105e723819744ed68a9bb22a2c23bf6f26572d
gduple <gduple@gmail.com>
2021-02-08 21:12:59 -0600

    measuring efficiency of processing strings

--- Chapter13/MonitoringApp/Program.cs
+++ Chapter13/MonitoringApp/Program.cs
@@ -9,17 +9,37 @@ namespace MonitoringApp
     {
         static void Main(string[] args)
         {
-            WriteLine( "Processing. Please wait..." );
+            // WriteLine( "Processing. Please wait..." );
+            // Recorder.Start();
+
+            // // simulate a process that requires some memory resources...
+            // int[] largeArrayOfInts =
+            //     Enumerable.Range(1, 10_000).ToArray();
+
+            // // ...and takes some time to complete
+            // System.Threading.Thread.Sleep(
+            //     new Random().Next( 5, 10 ) * 1000 );
+
+            // Recorder.Stop();
+
+            int[] numbers = Enumerable.Range(1, 50_000).ToArray();
+
+            WriteLine("Using string with +");
             Recorder.Start();
+            string s = "";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                s += numbers[i] + ", ";
+            }
+            Recorder.Stop();
 
-            // simulate a process that requires some memory resources...
-            int[] largeArrayOfInts =
-                Enumerable.Range(1, 10_000).ToArray();
-
-            // ...and takes some time to complete
-            System.Threading.Thread.Sleep(
-                new Random().Next( 5, 10 ) * 1000 );
-
+            WriteLine("Using StringBuilder");
+            Recorder.Start();
+            var builder = new System.Text.StringBuilder();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                builder.Append(numbers[i]); builder.Append(", ");
+            }
             Recorder.Stop();
         }
     }
