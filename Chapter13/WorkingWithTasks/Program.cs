67d9b1408c5dd7380f011ab9f2ed287d57ce8236
gduple <gduple@gmail.com>
2021-02-08 23:13:16 -0600

    continuing with another task

--- Chapter13/WorkingWithTasks/Program.cs
+++ Chapter13/WorkingWithTasks/Program.cs
@@ -16,11 +16,19 @@ namespace WorkingWithTasks
             // MethodB();
             // MethodC();
 
-            WriteLine("Running methods asynchronously on multiple threads.");
+            /* WriteLine("Running methods asynchronously on multiple threads.");
             Task taskA = new Task(MethodA);
             taskA.Start();
             Task taskB = Task.Factory.StartNew(MethodB);
             Task taskC = Task.Run(new Action(MethodC));
+            Task[] tasks = { taskA, taskB, taskC };
+            Task.WaitAll(tasks); */
+            WriteLine("Passing the result of one task as an input to another.");
+            var taskCallWebServiceAndThenStoredProcedure =
+                Task.Factory.StartNew(CallWebService)
+                    .ContinueWith(previousTask =>
+                        CallStoredProcedure(previousTask.Result));
+            WriteLine($"Result: {taskCallWebServiceAndThenStoredProcedure.Result}");
             WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
         }
 
@@ -43,6 +51,22 @@ namespace WorkingWithTasks
             WriteLine("Starting Method C...");
             Thread.Sleep(1000);
             WriteLine("Finished Method C.");
+        }
+
+        static decimal CallWebService()
+        {
+            WriteLine("Starting call to web service...");
+            Thread.Sleep((new Random()).Next(2000, 4000));
+            WriteLine("Finished call to web service.");
+            return 89.99M;
+        }
+
+        static string CallStoredProcedure(decimal amount)
+        {
+            WriteLine("Starting call to stored procedure...");
+            Thread.Sleep((new Random()).Next(2000, 4000));
+            WriteLine("Finished call to stored procedure.");
+            return $"12 products cost more than {amount:C}.";
         }
     }
 }
